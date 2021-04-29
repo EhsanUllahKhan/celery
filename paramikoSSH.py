d321de70062ebdf07cf9cc97e25caf43f41c9a3b
Ehsan Ullah Khan <ullahkhanihsan@yahoo.com>
2021-04-29 14:48:35 +0500

    changed command to execute on host

--- paramikoSSH.py
+++ paramikoSSH.py
@@ -5,7 +5,7 @@ import paramiko
 p = paramiko.SSHClient()
 p.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # This script doesn't work for me unless this line is added!
 p.connect("169.63.183.157", port=22, username="root", password="")
-stdin, stdout, stderr = p.exec_command("hostname")
+stdin, stdout, stderr = p.exec_command("ls -a")
 t.sleep(5)
 opt = stdout.readlines()
 opt = "".join(opt)
